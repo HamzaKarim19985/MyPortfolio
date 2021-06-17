3e55963fbb6425bb27a1025bfeab8556e9f85b27
HamzaKarim1998 <hamzakarim1998@gmail.com>
2021-06-17 14:14:33 -0400

    Avatar, Responsive desighn

---
+++ src/assets/data.js
@@ -0,0 +1,48 @@
+export const dataArray = [
+  {
+    title: "Cafeteria System",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQOaY7djEZQyTk_MJWmIM01c_x0ieCtv7CRBVeWMTFmefWaSa7g&usqp=CAU",
+    description:
+      "Made using React. Has registration/login modules, product list,menu,cart and Paypal. Firebase and Firestore used for back end.",
+    githubLink: "https://github.com/HamzaKarim19985/CafeteriaSystem",
+    demoLink: "https://login-3f01e.firebaseapp.com",
+  },
+  {
+    title: "E-commerce Website",
+    img: "https://i.pinimg.com/236x/ab/ca/4c/abca4c51c7e166b2980105b5e98b7ac2.jpg",
+    description:
+      "React based with all website components; navigation,Router links,modules, product list,menu,cart and Paypal.",
+    githubLink:
+      "https://github.com/HamzaKarim19985/react-phone-store-recording",
+    demoLink: "https://e-commerce-store-hk.netlify.com/",
+  },
+  {
+    title: "USER DATABASE (Node.js CRUD APP)",
+    img: "https://miro.medium.com/max/1320/1*y6C4nSvy2Woe0m7bWEn4BA.png",
+    description:
+      "React based website that can create, read, update, edit, search and delete user data that is retrieved from a custom JSON API server using HTTP methods",
+    githubLink: "https://github.com/HamzaKarim19985/DatabaseCRUDapp",
+    demoLink: "https://crud-userdata.netlify.com/",
+  },
+  {
+    title: "title",
+    img: "url",
+    description: "description",
+    githubLink: "link",
+    demoLink: "link",
+  },
+  {
+    title: "title",
+    img: "url",
+    description: "description",
+    githubLink: "link",
+    demoLink: "link",
+  },
+  {
+    title: "title",
+    img: "url",
+    description: "description",
+    githubLink: "link",
+    demoLink: "link",
+  },
+];
